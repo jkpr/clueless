80f0ecad87ca9389c592fd4be256cac953d61917
James Pringle <jameskpringle@gmail.com>
2016-11-22 13:45:47 -0500

    Support for Heroku

--- src/main/java/edu/jhu/boddybuilders/App.java
+++ src/main/java/edu/jhu/boddybuilders/App.java
@@ -8,8 +8,23 @@ import spark.Spark;
  */
 public class App 
 {
+    private static int views = 0;
+
     public static void main( String[] args )
     {
-        Spark.get("/", (req, res) -> "<h1>Hello SparkJava World!</h1>");
+        Spark.port(getHerokuAssignedPort());
+        Spark.get("/", (req, res) -> {
+            views++;
+            return "<h1>Hello SparkJava World!</h1><p>Views: " + String.valueOf(views) + "</p>";
+        });
+    }
+
+    // Found on https://sparktutorials.github.io/2015/08/24/spark-heroku.html
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
