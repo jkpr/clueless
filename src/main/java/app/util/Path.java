47fd8765bc10c6439f7e3601484e6bc6462f8988
James K. Pringle <jameskpringle@gmail.com>
2016-11-30 21:16:56 -0500

    Add URLs for POSTs that lead to Actions.

--- src/main/java/app/util/Path.java
+++ src/main/java/app/util/Path.java
@@ -17,6 +17,17 @@ public class Path {
         public static final String GAME = "/game/:id";
     }
 
+    public static class Action {
+        public static final String ADD_PLAYER = "/game/:id/add";
+        public static final String DISPROVE_SUGGESTION = "/game/:id/disprove";
+        public static final String END_TURN = "/game/:id/endturn";
+        public static final String MAKE_ACCUSATION = "/game/:id/accuse";
+        public static final String MAKE_SUGGESTION = "/game/:id/suggest";
+        public static final String MOVE = "/game/:id/move";
+        public static final String SET_TOKEN = "/game/:id/token";
+        public static final String START_GAME = "/game/:id/start";
+    }
+
     public static class Template {
         public static final String DIR = "/freemarker";
         public static final String INDEX = "/index.ftl";
