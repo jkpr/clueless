6a9fe256966d4564582d76133b600cc130ceaab1
James Pringle <jameskpringle@gmail.com>
2016-12-06 10:19:38 -0500

    GameModel test

--- src/test/java/app/GameModelTest.java
+++ src/test/java/app/GameModelTest.java
@@ -42,6 +42,23 @@ public class GameModelTest {
     @Test
     public void testPayload() {
         GameModel model = new GameModel();
+        try {
+            Player p1 = new Player(model.getBoard().getCharacter("Ms. Scarlet"));
+            Player p2 = new Player(model.getBoard().getCharacter("Mr. Green"));
+            Player p3 = new Player(model.getBoard().getCharacter("Prof. Plum"));
+            Player p4 = new Player(model.getBoard().getCharacter("Mrs. White"));
+            Player p5 = new Player(model.getBoard().getCharacter("Mrs. Peacock"));
+            Player p6 = new Player(model.getBoard().getCharacter("Col. Mustard"));
+            model.addPlayer(p1);
+            model.addPlayer(p2);
+            model.addPlayer(p3);
+            model.addPlayer(p4);
+            model.addPlayer(p5);
+            model.addPlayer(p6);
+            model.startGame();
+        } catch (GameModelException e) {
+            e.printStackTrace();
+        }
         GameModelPayload payload = model.toPayload();
         try {
             mapper.writeValue(System.out, payload);
